95cb6f0818fc5e5250236ef43eb8b1942a56b1b9
hiroshi fukuda <dada@sygnas.jp>
2017-12-04 12:45:45 +0800

    1.3.1 : ES5形式に変更

---
+++ dist/syg-throttle.es.js
@@ -0,0 +1,15 @@
+var index = function (timing, func, scope) {
+    var id = null;
+
+    return function () {
+        if (id !== null) return;
+        func.apply(scope);
+
+        id = setTimeout(function () {
+            id = null;
+        }, timing);
+    };
+};
+
+export default index;
+//# sourceMappingURL=syg-throttle.es.js.map
---
+++ dist/syg-throttle.js
@@ -0,0 +1,23 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global['syg-throttle'] = factory());
+}(this, (function () { 'use strict';
+
+var index = function (timing, func, scope) {
+    var id = null;
+
+    return function () {
+        if (id !== null) return;
+        func.apply(scope);
+
+        id = setTimeout(function () {
+            id = null;
+        }, timing);
+    };
+};
+
+return index;
+
+})));
+//# sourceMappingURL=syg-throttle.js.map
