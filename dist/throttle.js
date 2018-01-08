4f6deb157c255bd10782edf9d390f13297096ff4
hiroshi fukuda <dada@sygnas.jp>
2018-01-08 15:49:34 +0900

    package.json の moduleName を復活。

--- dist/throttle.js
+++ dist/throttle.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
-	(global.throttle = factory());
+	(global['syg-throttle'] = factory());
 }(this, (function () { 'use strict';
 
 var index = function (timing, func, scope) {
